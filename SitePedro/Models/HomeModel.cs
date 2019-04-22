c211e0aec706d6788b8b0abd7623aff255d122ff
PedroLavadinho <pedro.gbl1998@gmail.com>
2019-04-22 15:45:17 +0100

    HomeModel para automatizar textbox do slogan da página Home.

---
+++ SitePedro/Models/HomeModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Web;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web;
+using Umbraco.ModelsBuilder;
+using Umbraco.ModelsBuilder.Umbraco;
+using Umbraco.Web.Models;
+using Umbraco.Core;
+using System.Web.Mvc;
+using System.Drawing;
+
+namespace SitePedro.Models
+{
+    public class Home : ContentModel
+    {
+        public string Slogan
+        {
+            get { return Content.Value<string>("slogan"); }
+        }
+
+
+        public Home(IPublishedContent content) : base(content)
+        {
+
+        }
+
+
+
+    }
+}
